f282092ee340c047fc7d131733cf6a1dd88a8ae0
petrinivictor <ciurceadaniel@gmail.com>
2020-03-29 22:41:56 +0300

    OperatiiMatrice

---
+++ OperatiiCuMatrice.m
@@ -0,0 +1,14 @@
+format compact;
+clc;
+clear;
+close all;
+A=[1 2 5;3 4 8;5 6 10];
+B=[1 5 -3;-2 5 45;4 8 6];
+A-B      %Diferenta a doua matrice
+A+B      %Suma a doua matrice
+A^-1     %Inversa unei matrice
+A'       %Transpusa unei matrice
+C=[A;B]  %Concatenare
+
+%Pentru a folosi si a aparea in CommandWindow o singura operatie puneti ;
+%la sfarsitul liniilor ce corespund operatiilor nedorite
